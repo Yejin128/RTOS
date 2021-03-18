41a58399eeb143eb9efa1d10ec455c44bcebf334
Yejin128 <bbaekhuen@gmail.com>
2021-03-17 21:46:02 -0700

    Add-TaskControlBlock

--- boot/Main.c
+++ boot/Main.c
@@ -4,9 +4,13 @@
 #include "stdio.h"
 #include "HalTimer.h"
 #include "stdlib.h"
+#include "task.h"
 
 static void Timer_test(void);
 static void Hw_init(void);
+void User_task0(void);
+void User_task1(void);
+void User_task2(void);
 
 void main(void)
 {
@@ -45,4 +49,56 @@ static void Timer_test(void)
         putstr("tic\n");
         delay(1000);
     }
+}
+
+static void Kernel_init(void)
+{
+    uint32_t taskId;
+
+    Kernel_task_init();
+
+    taskId = Kernel_task_create(User_task0);
+    if (NOT_ENOUGH_TASK_NUM == taskId)
+    {
+        putstr("Task0 creation fail\n");
+    }
+
+    taskId = Kernel_task_create(User_task1);
+    if (NOT_ENOUGH_TASK_NUM == taskId)
+    {
+        putstr("Task1 creation fail\n");
+    }
+
+    taskId = Kernel_task_create(User_task2);
+    if (NOT_ENOUGH_TASK_NUM == taskId)
+    {
+        putstr("Task2 creation fail\n");
+    }
+}
+
+void User_task0(void)
+{
+    uint32_t local = 0;
+
+    putstr("User Task #0\n");
+
+    while(true);
+}
+
+void User_task1(void)
+{
+    uint32_t local = 0;
+
+    putstr("User Task #1\n");
+
+    while(true);
+}
+
+void User_task2(void)
+{
+    uint32_t local = 0;
+
+    putstr("User Task #2\n");
+
+    while(true);
 }
